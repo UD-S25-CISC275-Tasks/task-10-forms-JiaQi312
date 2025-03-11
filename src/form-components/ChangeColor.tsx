70bc06e2476c926f3511cc464c6b5d19d2a12f21
JiaQi312 <148893867+JiaQi312@users.noreply.github.com>
2025-03-10 23:25:03 -0400

    still working on change color

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -13,12 +13,6 @@ const COLORS: string[] = [
     "magenta",
 ];
 
-//color interface to pass in state
-interface colorClass {
-    color: string;
-    setColor: (color: string) => void;
-}
-
 export function ChangeColor(): React.JSX.Element {
     const [color, setColor] = useState<string>(COLORS[0]);
 
@@ -27,26 +21,34 @@ export function ChangeColor(): React.JSX.Element {
         setColor(event.target.value);
     }
 
+    //styling for box
+    const boxStyle = {
+        backgroundColor: color,
+    };
+
     return (
         <div>
             <h3>Change Color</h3>
             {/* map method to create button for each color, which runs updateColor on click */}
-            {COLORS.map((currColor: string) => (
-                <Form.Check
-                    type="radio"
-                    name="colorSelect"
-                    onChange={updateColor}
-                    id={currColor}
-                    key={currColor}
-                    value={color}
-                    checked={color === currColor}
-                />
-            ))}
+            {COLORS.map(
+                (currColor: string) =>
+                    (
+                        <Form.Check
+                            type="radio"
+                            name="colorSelect"
+                            onChange={updateColor}
+                            id={currColor}
+                            key={currColor}
+                            value={currColor}
+                            checked={color === currColor}
+                        />
+                    ) + currColor,
+            )}
             {/* text to display the final color */}
             {
                 <span>
-                    You have chosen{" "}
-                    <span data-testid="colored-box" background-color={color}>
+                    You have chosen + " "
+                    <span data-testid="colored-box" style={boxStyle}>
                         {color}
                     </span>
                 </span>
